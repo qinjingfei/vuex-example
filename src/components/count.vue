3e0771ae70678636f75a9911b95bdbab413a7a8e
Jingfei Qin <qinjingfei1@gmail.com>
2018-09-11 11:11:52 +0800

    计数器（不用vuex）

---
+++ src/components/count.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>计数器</h1>
+        <h2>{{count}}</h2>
+        <button @click="increase">+</button>
+        <button @click="decrease">-</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            count:0
+        }
+    },
+    methods: {
+        increase() {
+            this.count++
+        },
+        decrease(){
+            if(this.count!==0) this.count--
+            else alert(`Already 0`)
+        }
+    }
+}
+</script>
+
+ <style scoped>
+    div{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+ </style>
+ 
+
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Count from './components/count.vue'
 
 Vue.use(Router)
 
@@ -18,6 +19,14 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/count',
+      name: 'count',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: Count
     }
   ]
 })
